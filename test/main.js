bf9beb5e843143bcbb368c005d4c074507608d40
roserinter <rkunnnnnn@gmail.com>
2024-10-08 03:51:10 +0000

    10月8日　実習

---
+++ test/main.js
@@ -0,0 +1,16 @@
+//main.js
+/*let count=0;
+function increaseCount(){
+    if(count==10){
+        count=-1;
+    }
+    count++;
+    document.getElementById("counter").innerText=count;
+}
+function herasuCount(){
+    if(count==0){
+        count=1;
+    }
+    count--;
+    document.getElementById("counter").innerText=count;
+}*/
